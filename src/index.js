a6f2cfccd8857a6993224309390840450d11175e
ivy.cocharo <ivy.cocharo@whitecloak.com>
2020-03-04 10:49:59 +0800

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk'
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(
+ReactDOM.render( 
   <Provider store={store}>
     <App />
   </Provider>,
